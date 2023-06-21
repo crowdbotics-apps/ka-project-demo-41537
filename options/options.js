95e5b3a272c609ec5c84ebced4282d93cfd4c464
Slobodan Petrovic <slobodan.petrovic@crowdbotics.com>
2023-06-21 19:54:56 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "ka-project-demo-41537",
-  url: "https://ka-project-demo-41537.botics.co",
-  api: "https://ka-project-demo-41537.botics.co/api/v1"
+  "name": "ka-project-demo-41537",
+  "url": "https://ka-project-demo-41537.botics.co",
+  "api": "https://ka-project-demo-41537.botics.co/api/v1",
+  "initialRoute": "Onboarding"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
